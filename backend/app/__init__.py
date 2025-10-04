df015454d16bd660f6973f17adce45537ae04b45
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-10-04 00:01:46 -0400

    reset repo

--- backend/app/__init__.py
+++
@@ -1,2 +0,0 @@
-# Domain modules package
-
--- backend/app/users/router.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter(prefix="/users", tags=["users"])
-
-@router.get("/")
-async def get_users():
-    return {"users": []}
-
